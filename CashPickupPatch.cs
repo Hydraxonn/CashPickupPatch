0a5d30954210e1122069e2183abdab26e4c143aa
Hayden Purificato <hayden.purificato@gmail.com>
2025-06-29 18:11:04 -0400

    Initial Commit

---
+++ CashPickupPatch.cs
@@ -0,0 +1,64 @@
+using System;
+using GTA;
+using GTA.Native;
+public class CashPickupPatch : Script
+{
+    Random rng = new Random();
+    int add = 0;
+    public CashPickupPatch()
+    {
+        Tick += OnTick;
+        Interval = 500;
+    }
+    private void OnTick(object sender, EventArgs e)//this is horrendously unoptimized, just a POC
+    {
+        if (Game.Player.Character.Model.Hash != new Model("player_zero").Hash && Game.Player.Character.Model.Hash != new Model("player_one").Hash && Game.Player.Character.Model.Hash != new Model("player_two").Hash)//if not M F or T
+        { 
+            foreach (Prop pickup in World.GetAllPickupObjects())
+            {
+                if (pickup != null)
+                {
+                    float dist = Game.Player.Character.Position.DistanceTo(pickup.Position);
+                    if (dist < 2)
+                    {
+                        switch (pickup.Model.Hash)
+                        {
+                            case -295781225: //prop_cash_pile_01
+                                add = rng.Next(500, 2000);
+                                //GTA.UI.Notification.Show("Pickup detected - Cash Pile");
+                                Game.Player.Money += add;
+                                pickup.Delete();
+                                break;
+                            case 880595258: //prop_ld_case_01
+                                add = rng.Next(2000, 5000);
+                               // GTA.UI.Notification.Show("Pickup detected - Briefcase");
+                                Game.Player.Money += add;
+                                pickup.Delete();
+                                break;
+                            case -1379254308: //Prop_LD_Wallet_01
+                                add = rng.Next(500, 2000);
+                                //GTA.UI.Notification.Show("Pickup detected - Wallet");
+                                Game.Player.Money += add;
+                                pickup.Delete();
+                                break;
+                            case -1666779307: //p_poly_bag_01_s
+                                add = rng.Next(5000, 12000);
+                                //GTA.UI.Notification.Show("Pickup detected - Cash Bag");
+                                Function.Call(Hash.PLAY_SOUND, -1, "PICKUP_DEFAULT", "HUD_FRONTEND_STANDARD_PICKUPS_SOUNDSET");
+                                Game.Player.Money += add;
+                                pickup.Delete();
+                                break;
+                            case -1249748547: //prop_security_case_01
+                                add = rng.Next(7000, 20000);
+                                //GTA.UI.Notification.Show("Pickup detected - Security Case");
+                                Function.Call(Hash.PLAY_SOUND, -1, "PICKUP_DEFAULT", "HUD_FRONTEND_STANDARD_PICKUPS_SOUNDSET");
+                                Game.Player.Money += add;
+                                pickup.Delete();
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("ThrowProps")]
+[assembly: AssemblyDescription("Pick up and Throw Props like in GTA 4")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hydraxonn")]
+[assembly: AssemblyProduct("ThrowProps")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("12a03976-1077-4770-9ffd-e6caa6681e0b")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
